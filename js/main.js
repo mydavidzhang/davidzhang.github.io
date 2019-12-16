737e1c052ba90eff42f9c6ca47820e7da1574d0b
David Zhang <david.zhang07@sap.com>
2019-12-16 11:35:15 -0800

    second

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Make sure sw are supported
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw_cached_site.js')
+        navigator.serviceWorker.register('../davidzhang.github.io/sw_cached_site.js')
             .then(reg => console.log('sw registered'))
             .catch(err => console.log(`sw error: ${err}`))
     });
