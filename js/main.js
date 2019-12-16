3e4e137553521a9f9c56fab183e80ac3f89317e9
David Zhang <david.zhang07@sap.com>
2019-12-16 11:45:12 -0800

    Add comment for demain name issue

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //Make sure sw are supported
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
+        //Need change to ../sw_cached_site.js for local
+        //Need change to ../davidzhang.github.io/sw_cached_site.js for github page https://mydavidzhang.github.io/davidzhang.github.io/.
         navigator.serviceWorker.register('../davidzhang.github.io/sw_cached_site.js')
             .then(reg => console.log('sw registered'))
             .catch(err => console.log(`sw error: ${err}`))
